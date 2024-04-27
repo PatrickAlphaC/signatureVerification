12d62cb9398f29c725db7555f7343dd5d5aa696d
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2024-04-27 15:28:33 -0400

    added examples of different ways to encode signatures

--- src/SignatureVerifierWithOZ.sol
+++ src/SignatureVerifierWithOZ.sol
@@ -9,6 +9,13 @@ contract SignatureVerifier {
         bytes32 hashedMessage = bytes32(message);
         (address signer, /*ECDSA.RecoverError recoverError*/, /*bytes32 signatureLength*/ ) =
             ECDSA.tryRecover(hashedMessage, _v, _r, _s);
+
+        // The above is equivalent to each of the following:
+        // address signer = ECDSA.recover(hashedMessage, _v, _r, _s);
+        // address signer = ecrecover(hashedMessage, _v, _r, _s);
+
+        // bytes memory packedSignature = abi.encodePacked(_r, _s, _v); // <-- Yes, the order here is different!
+        // address signer = ECDSA.recover(hashedMessage, packedSignature);
         return signer;
     }
 
